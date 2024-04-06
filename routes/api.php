c5940b853ac94a1e42eac36a9e98ec66a4c17c5d
AlessioPirrotta <alessiopirrotta92@gmail.com>
2024-04-06 19:39:08 +0200

    first commit

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,11 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('/test', function (){
+    return response()->json([
+        'name'=> 'marco',
+        'state'=> 'Dussel'
+    ]);
+});
+
 
