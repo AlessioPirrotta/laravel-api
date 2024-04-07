3c1c68aa70a5d95fede97c1eaec39fc3ff5a6f89
AlessioPirrotta <alessiopirrotta92@gmail.com>
2024-04-07 20:34:29 +0200

    exercise completed Api

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    public function index(){
+public function index(){
+    $projects= Project::all();
 
-    }
+
+    return response()->json([
+        'succes'=> true,
+        'projects'=> $projects,
+    ]);
+}
 }
